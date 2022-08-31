ea1c988a2e81f7f4ba2aebe0bd30b46b60cd41c3
Víctor Santiago Ferrera <victor.santiago@the-cocktail.com>
2022-08-31 10:28:13 +0200

    Reto2 UF2
    Algoritmo para comprobar si un número es primo o no.

--- NumerosPrimos/src/NumerosPrimos.java
+++ NumerosPrimos/src/NumerosPrimos.java
@@ -3,20 +3,28 @@ import java.util.Scanner;
 public class NumerosPrimos {
     public static void main(String[] args) throws Exception {
 
+        Scanner input = new Scanner(System.in);
         int num = 0;
         int i = 0;
-        Scanner input = new Scanner(System.in);
+        boolean esPrimo = true;
 
-        System.out.print("Introduzca un número para comprobar si es primo: ");
+        System.out.println("Introduce un número:");
         num = input.nextInt();
-        // TODO
+
+        // Bucle que recorra desde un número menos al introducido hasta el 2.
         for (i = num - 1; i >= 2; i--) {
+
             if (num % i == 0) {
-                System.out.println("Es primo");
-            } else {
-                System.out.println("No es primo");
+                // No es primo
+                esPrimo = false;
+                break; // Para que al encontrar el primero que sea divisible se pare el bucle.
             }
         }
 
+        if (esPrimo) {
+            System.out.println("El número es primo");
+        } else {
+            System.out.println("El número NO es primo");
+        }
     }
 }
